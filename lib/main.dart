b880acee57fdfd67633b5ed7e27f8097902ead16
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-12-03 21:33:14 +0700

    Made landing page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:mid_tourism_mobile/drawer.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Homepage',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Homepage'),
@@ -32,16 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,24 +31,43 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      drawer: const drawerapp(),
-      body: const Center(
-        child: Text(
-          "something"
-        ),
-      ),
-    ); // This trailing comma makes auto-formatting nicer for build methods
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text("MID Tourism"),
+              Text("Go Anywhere, Know Everywhere\n Experience authenticity and accomodation with MID-Tourism"),
+              TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.red),
+                          ),
+                          onPressed: () {
+          
+                          },
+                          child: const Text(
+                            'Log-in',
+                            
+                          )),
+              TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.red),
+                          ),
+                          onPressed: () {
+          
+                          },
+                          child: const Text(
+                            'Register',
+                            
+                          )),
+            ]
+          )
+        )
+      )
+    );
   }
 }
