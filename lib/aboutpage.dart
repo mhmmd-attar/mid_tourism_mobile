6f0d6180da28cd3ff006e11975833f1d4e5e794a
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-12-04 15:12:45 +0700

    Added about page

---
+++ lib/aboutpage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:mid_tourism_mobile/drawer.dart';
+
+
+class MyAboutApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'About',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyAboutPage(title: 'About'),
+    );
+  }
+}
+
+class MyAboutPage extends StatefulWidget {
+  const MyAboutPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyAboutPage> createState() => _MyAboutPageState();
+}
+
+class _MyAboutPageState extends State<MyAboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blue,
+            body: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0,50,0,0),
+                    child: Text("MID Tourism"),
+                  ),
+                  Container(
+                    child: Text("Go Anywhere, Know Everywhere \n Experience authenticity and accomodation with MID-Tourism"),
+                  ),
+                  Container(
+                    child: Text("Learn more about MID Tourism")
+                  ),
+
+                  Container(
+                        child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Colors.red),
+                            ),
+                            onPressed: () {
+                            },
+                            child: const Text('Log-in!',)
+                        ),
+                      ),
+                  Container(
+                        child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(Colors.red),
+                            ),
+                            onPressed: () {
+                            },
+                            child: const Text('Register',)
+                        ),
+                      ),
+                  
+
+                  // Added behavior when budget is typed
+                ],
+              ),
+            )
+        )
+    );
+  }
+}
