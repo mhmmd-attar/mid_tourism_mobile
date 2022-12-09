bdefe32bdf62f870b5bf1faa31790ea27840dabf
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-12-09 22:22:52 +0700

    rental transport

--- lib/pages/transportation/transport.dart
+++ lib/pages/transportation/transport.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mid_tourism_mobile/drawer.dart';
 import 'package:mid_tourism_mobile/pages/transportation/transportform.dart';
+import 'package:mid_tourism_mobile/pages/homepage/login.dart';
+import 'package:mid_tourism_mobile/pages/homepage/register.dart';
+import 'package:mid_tourism_mobile/models/transportModel.dart';
 
 class TransportPage extends StatefulWidget {
   const TransportPage({super.key});
@@ -19,15 +22,126 @@ class _Transport extends State<TransportPage> {
       drawer: const drawerapp(),
       body: Center(
         child: TextButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const TransportForm()),
-            );
-          },
-          child: const Text('Add Transport'),
-        ),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const TransportForm()),
+              );
+            },
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.all(20.0),
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("rentals.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // ignore: prefer_const_constructors
+                            FittedBox(
+                              fit: BoxFit.fitWidth,
+                              child: const Text("Rental Transportations",
+                                  style: TextStyle(
+                                      fontSize: 60,
+                                      fontFamily: 'Quicksand',
+                                      color: Colors.white)),
+                            ),
+                            Center(
+                              child: Text("",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontFamily: 'Quicksand',
+                                      color: Colors.white.withOpacity(0.8))),
+                            ),
+                            Center(
+                              child: Text("Vehicles you can rent!",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontFamily: 'Quicksand',
+                                      color: const Color(0xffFFFFFF)
+                                          .withOpacity(0.8))),
+                            ),
+                          ])),
+                  FutureBuilder(
+                      future: TransportFuture().fetchTransport(),
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (snapshot.data == null) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else {
+                          if (!snapshot.hasData) {
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "No Vehicles Registered :(",
+                                  style: TextStyle(
+                                      color: Color(0xff59A5D8), fontSize: 20),
+                                ),
+                                SizedBox(height: 8),
+                              ],
+                            );
+                          } else {
+                            return ListView.builder(
+                                itemCount: snapshot.data!.length,
+                                itemBuilder: (_, index) => Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 12),
+                                    padding: const EdgeInsets.all(20.0),
+                                    decoration: BoxDecoration(
+                                        color: Colors.blue,
+                                        borderRadius:
+                                            BorderRadius.circular(15.0),
+                                        boxShadow: const [
+                                          BoxShadow(
+                                              color: Colors.black,
+                                              blurRadius: 2.0)
+                                        ]),
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            "${snapshot.data![index].fields.itemTitle}",
+                                            style: const TextStyle(
+                                              fontSize: 18.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ])));
+                          }
+                        }
+                      }),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const TransportForm()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff24a0ed),
+                            shape: const StadiumBorder(),
+                            minimumSize: const Size(260, 50)),
+                        child: const Text("Add Vehicle",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Quicksand',
+                                color: Color(0xffFFFFFF)))),
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
--- lib/pages/transportation/transportform.dart
+++ lib/pages/transportation/transportform.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mid_tourism_mobile/drawer.dart';
+import 'package:mid_tourism_mobile/models/hotelModel.dart';
+import 'package:mid_tourism_mobile/pages/transportation/transportform.dart';
+
+enum booleanEnum { yes, no }
 
 class TransportForm extends StatefulWidget {
   const TransportForm({super.key});
@@ -10,17 +14,174 @@ class TransportForm extends StatefulWidget {
 
 class _TransportForm extends State<TransportForm> {
   final _formKey = GlobalKey<FormState>();
+  String companyName = "";
+  String transportName = "";
+  String transportPrice = "";
+  String description = "";
+  bool availability = false;
+  int pk = 1;
+  booleanEnum? _booleanEnum;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Transportation Form'),
-      ),
-      drawer: const drawerapp(),
-      body: const Center(
-        child: Text('this is a form'),
-      )
-    );
+        appBar: AppBar(
+          title: const Text('Hotel Form'),
+        ),
+        drawer: const drawerapp(),
+        body: SingleChildScrollView(
+            child: Center(
+                child: Column(children: <Widget>[
+          Container(
+              padding: const EdgeInsets.all(5),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // ignore: prefer_const_constructors
+                  Container(
+                      padding: const EdgeInsets.only(left: 15, right: 15),
+                      child: const FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text("Transportations",
+                            style: TextStyle(
+                                fontSize: 60,
+                                fontFamily: 'Quicksand',
+                                color: Colors.black)),
+                      )),
+                  Center(
+                    child: Text("",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'Quicksand',
+                            color: Colors.black.withOpacity(0.5))),
+                  ),
+                  Center(
+                    child: Text("",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'Quicksand',
+                            color: const Color(0xff000000).withOpacity(0.5))),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Container(
+                      padding: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            // Using padding of 8 pixels
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Enter Company Name",
+                                labelText: "Company Name",
+                                // Added a circular border to make it neater
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                              ),
+                              // Added behavior when name is typed
+                              onChanged: (String? value) {
+                                setState(() {
+                                  companyName = value!;
+                                });
+                              },
+                              // Added behavior when data is saved
+                              onSaved: (String? value) {
+                                setState(() {
+                                  companyName = value!;
+                                });
+                              },
+                              // Validator as form validation
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Company Name cannot be empty!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            // Using padding of 8 pixels
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Enter your Vehicle Type",
+                                labelText: "Vehicle Type",
+                                // Added a circular border to make it neater
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                              ),
+                              // Added behavior when name is typed
+                              onChanged: (String? value) {
+                                setState(() {
+                                  transportName = value!;
+                                });
+                              },
+                              // Added behavior when data is saved
+                              onSaved: (String? value) {
+                                setState(() {
+                                  transportName = value!;
+                                });
+                              },
+                              // Validator as form validation
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Hotel address cannot be empty!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            // Using padding of 8 pixels
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Enter the price",
+                                labelText: "Price",
+                                // Added a circular border to make it neater
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                              ),
+                              // Added behavior when name is typed
+                              onChanged: (String? value) {
+                                setState(() {
+                                  transportPrice = value!;
+                                });
+                              },
+                              // Added behavior when data is saved
+                              onSaved: (String? value) {
+                                setState(() {
+                                  transportPrice = value!;
+                                });
+                              },
+                              // Validator as form validation
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Price cannot be empty!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ))
+        ]))));
   }
 }
